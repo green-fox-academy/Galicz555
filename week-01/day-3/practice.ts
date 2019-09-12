b79e55752de3e9a28e2aca1d9f8b33cf61ba1196
Galicz Mihály <galicz555@gmail.com>
2019-09-12 14:11:27 +0200

    excercise favorite number

--- week-01/day-3/practice.ts
+++ week-01/day-3/practice.ts
@@ -45,3 +45,11 @@ console.log(codingHours)
 let workHours: number = 17*52
 let percentage: number = (codingHours/workHours*100);
 console.log(percentage);
+
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+let favoriteNumber: number = 5;
+
+console.log("'My favorite number is: " + favoriteNumber +"'");
