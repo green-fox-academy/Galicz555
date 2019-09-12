a0b852757be17590286ec506eeba806b4cd0554b
Galicz Mihály <galicz555@gmail.com>
2019-09-12 12:16:43 +0200

    Deleted unnecessary things

--- week-01/day-3/practice.ts
+++ week-01/day-3/practice.ts
@@ -1,24 +1,13 @@
+// 2nd excercise
+
 console.log('Humpty Dumpty sat on a wall,');
 console.log('Humpty Dumpty had a great fall.');
 console.log('All the king\'s horses and all the king\'s men');
 console.log('Couldn\'t put Humpty together again.');
 
-//3rd excersice
+// 3rd excersice
 
 console.log("Hello, Daniel!");
 console.log("Hello, Gábor");
 console.log("Hello Kriszti");
 
-/*
-mfskjnfaosdnfksafnksnfsfn
-*/
-
-console.log('Hello, World!');
-console.log(42);
-console.log(3.123123123);
-console.log(true);
-
-console.log(42); // Prints 42
-console.log(-1); // Prints -1
-console.log(0); // Prints 0
-console.log(12341234123412341234);
