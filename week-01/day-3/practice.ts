4fd06f094c729c9254d93d90288b0dcd2a635ef0
Galicz Mihály <galicz555@gmail.com>
2019-09-13 10:36:49 +0200

    Multiplication table exercise

--- week-01/day-3/practice.ts
+++ week-01/day-3/practice.ts
@@ -258,9 +258,33 @@ console.log(out3);
 for (let i: number = 0; i < 100; i++){
     console.log("I won't cheat on the exam!");
 }
-*/
+
 
 // Create a program that prints all the even numbers between 0 and 500
 for (let i: number = 0; i <= 500; i++){
     console.log(i++);
+}
+
+*/
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+
+let x: number = 3;
+for (let i: number = 0; i <=10; i++){
+    console.log(i * x);
 }
