ff9ea129d7979f66c1aa42650531fabb750f28e2
Galicz Mihály <galicz555@gmail.com>
2019-09-18 17:24:23 +0200

    Finished Drawing exercise

--- week-02/day-3/16. Starry night/starryNights.ts
+++ week-02/day-3/16. Starry night/starryNights.ts
@@ -11,3 +11,12 @@ const ctx = canvas.getContext('2d');
 //  - The stars should be small squares
 //  - The stars should have random positions on the canvas
 //  - The stars should have random color (some shade of grey)
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+for(let i=0; i<99; i++){
+ctx.fillStyle = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+ctx.fillRect(Math.random()*600, Math.random()*400, 5, 5);
+}
+
