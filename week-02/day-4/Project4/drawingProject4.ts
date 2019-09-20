0c97d8f0cce8c2aa6f5f84ef0de9e114a1cd2016
Galicz Mihály <galicz555@gmail.com>
2019-09-20 10:30:20 +0200

    Project 4: code cleared up

--- week-02/day-4/Project4/drawingProject4.ts
+++ week-02/day-4/Project4/drawingProject4.ts
@@ -10,15 +10,14 @@ const ctx = canvas.getContext('2d');
 function Pyramid(x, y){
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(x + width,y);
+    ctx.lineTo(x + width, y);
     ctx.lineTo(x + width / 2, y - height);
     ctx.lineTo(x, y);
     ctx.stroke();
 }
 
-// Pyramid(250,30);
 let width = 20;
-let height = Math.sqrt((width**2)-(width/2)**2);
+let height = Math.sqrt((width**2) - (width/2) ** 2);
 let lineCount = 25;
 let startingWidth = 240;
 let startingHeight = 30;
@@ -28,7 +27,7 @@ let startingHeight = 30;
 
 for(let i = 0; i < lineCount; i++){
     for(let j = 0; j <= i ; j++){
-        Pyramid(startingWidth+j*width, startingHeight);
+        Pyramid(startingWidth + j * width, startingHeight);
     }
     startingWidth -= width/2;
     startingHeight += height;
