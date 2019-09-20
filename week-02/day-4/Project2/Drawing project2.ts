bfd6b3e0e959d61759f54ebcb89039fefeee5437
Galicz Mihály <galicz555@gmail.com>
2019-09-20 10:24:33 +0200

    Project 2: code cleared up

--- week-02/day-4/Project2/Drawing project2.ts
+++ week-02/day-4/Project2/Drawing project2.ts
@@ -3,37 +3,35 @@ const ctx = canvas.getContext('2d');
 
 
 let checksize = 1;
-let dense = 5;
+let spacing = 10;
 
 
 let newCanvasWidth = canvas.width/checksize;
-let startingWidth = 0;
-let startingHeight = 0;
+let startingWidthPoint = 0;
+let startingHeightPoint = 0;
 
 
-function drawRectLineplay(x,y,z){
+function drawRectLineplay(x, y, z){
     ctx.fillRect(x, y, z, z);
 
-    function drawLineplay (x,y){
+    function drawLineplay (x, y){
         ctx.strokeStyle = 'green';
-        for(let i = 0; i < newCanvasWidth; i+=dense){
+        for(let i = 0; i < newCanvasWidth; i += spacing){
             ctx.beginPath();
-            ctx.moveTo(x, y+i);
-            ctx.lineTo(x+i,y+newCanvasWidth);
+            ctx.moveTo(x, y + i);
+            ctx.lineTo(x + i, y + newCanvasWidth);
             ctx.stroke();
-            // }
         }
     }
     drawLineplay (x, y);
     
-    function drawLineplay2 (x,y){
+    function drawLineplay2 (x, y){
         ctx.strokeStyle = 'green';
-        for(let i = 0; i < newCanvasWidth; i+=dense){
+        for(let i = 0; i < newCanvasWidth; i += spacing){
             ctx.beginPath();
-            ctx.moveTo(x+i, y);
-            ctx.lineTo(x+newCanvasWidth, y+i);
+            ctx.moveTo(x + i, y);
+            ctx.lineTo(x + newCanvasWidth, y + i);
             ctx.stroke();
-            // }
         }
     }
     drawLineplay2 (x, y);
@@ -41,8 +39,8 @@ function drawRectLineplay(x,y,z){
 
 for(let i = 0; i < checksize; i++){
     for( let j = 0; j < checksize; j++){
-        drawRectLineplay(startingWidth+j*newCanvasWidth,startingHeight,newCanvasWidth);
+        drawRectLineplay(startingWidthPoint + j * newCanvasWidth, startingHeightPoint, newCanvasWidth);
     }
-    startingHeight += newCanvasWidth;
+    startingHeightPoint += newCanvasWidth;
 }
 
