f875854f03e83f4fe53cece0e45da3f147d0fd1d
Galicz Mihály <galicz555@gmail.com>
2019-09-20 10:26:29 +0200

    Project 1: code cleared up

--- week-02/day-4/Drawing project.ts
+++ week-02/day-4/Drawing project.ts
@@ -6,28 +6,24 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 
-function drawNiceThing (x,y,a,b){
+function drawNiceThing (x, y, a, b){
     ctx.strokeStyle = 'green';
-    for(let i = 0; i < canvas.width; i+=20){
-        // for(let k = 0; i < canvas.height; k+=20){
+    for(let i = 0; i < canvas.width; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x-i, y);
-        ctx.lineTo(a, b-i);
+        ctx.moveTo(x - i, y);
+        ctx.lineTo(a, b - i);
         ctx.stroke();
-        // }
     }
 }
 drawNiceThing (500, 500, 0, 500);
 
-function drawNiceThing2 (x,y,a,b){
+function drawNiceThing2 (x, y, a, b){
     ctx.strokeStyle = 'purple';
-    for(let i = 0; i < canvas.width; i+=20){
-        // for(let k = 0; i < canvas.height; k+=20){
+    for(let i = 0; i < canvas.width; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x+i, y);
-        ctx.lineTo(a, b+i);
+        ctx.moveTo(x + i, y);
+        ctx.lineTo(a, b + i);
         ctx.stroke();
-        // }
     }
 }
 drawNiceThing2 (0, 0, 500, 0);
