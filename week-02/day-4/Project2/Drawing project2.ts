2c29bf6d27ec377320376eaea584f6cc0dbb116f
Galicz Mihály <galicz555@gmail.com>
2019-09-20 10:13:56 +0200

    Project 2: Finished !!!

--- week-02/day-4/Project2/Drawing project2.ts
+++ week-02/day-4/Project2/Drawing project2.ts
@@ -2,21 +2,21 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 
-let checksize = 4;
-let dense = 5
+let checksize = 1;
+let dense = 5;
 
 
 let newCanvasWidth = canvas.width/checksize;
+let startingWidth = 0;
+let startingHeight = 0;
+
 
 function drawRectLineplay(x,y,z){
     ctx.fillRect(x, y, z, z);
-    ctx.strokeStyle = "white";
-    ctx.strokeRect(x, y, z, z);
 
-    function drawNiceThing (x,y){
+    function drawLineplay (x,y){
         ctx.strokeStyle = 'green';
         for(let i = 0; i < newCanvasWidth; i+=dense){
-            // for(let k = 0; i < canvas.height; k+=20){
             ctx.beginPath();
             ctx.moveTo(x, y+i);
             ctx.lineTo(x+i,y+newCanvasWidth);
@@ -24,12 +24,11 @@ function drawRectLineplay(x,y,z){
             // }
         }
     }
-    drawNiceThing (x, y);
+    drawLineplay (x, y);
     
-    function drawNiceThing2 (x,y){
-        ctx.strokeStyle = 'purple';
+    function drawLineplay2 (x,y){
+        ctx.strokeStyle = 'green';
         for(let i = 0; i < newCanvasWidth; i+=dense){
-            // for(let k = 0; i < canvas.height; k+=20){
             ctx.beginPath();
             ctx.moveTo(x+i, y);
             ctx.lineTo(x+newCanvasWidth, y+i);
@@ -37,16 +36,13 @@ function drawRectLineplay(x,y,z){
             // }
         }
     }
-    drawNiceThing2 (x, y);
+    drawLineplay2 (x, y);
 }
 
-drawRectLineplay(0,0,newCanvasWidth);
+for(let i = 0; i < checksize; i++){
+    for( let j = 0; j < checksize; j++){
+        drawRectLineplay(startingWidth+j*newCanvasWidth,startingHeight,newCanvasWidth);
+    }
+    startingHeight += newCanvasWidth;
+}
 
-
-
-// let checkSize: number = 20;
-// for(let i = 0; i < checkSize * 600; i++){
-//     for(let j = 0; j < 50; j++){
-//         drawRectLineplay(i*checkSize,j*checkSize,1*checkSize);
-//     }
-// }
