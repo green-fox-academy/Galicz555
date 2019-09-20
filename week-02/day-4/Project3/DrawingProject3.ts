06165901be28fb1c55444134e5e901dbbc003149
Galicz Mihály <galicz555@gmail.com>
2019-09-20 10:32:33 +0200

    Project 3: code cleared up

--- week-02/day-4/Project3/DrawingProject3.ts
+++ week-02/day-4/Project3/DrawingProject3.ts
@@ -6,45 +6,45 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 
-function drawNiceThing (x,y,a,b){
+function drawNiceThing (x, y, a, b){
     ctx.strokeStyle = 'green';
-    for(let i = 0; i < canvas.width/2; i+=20){
+    for(let i = 0; i < canvas.width / 2; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x-i, y);
-        ctx.lineTo(a, b-i);
+        ctx.moveTo(x - i, y);
+        ctx.lineTo(a, b - i);
         ctx.stroke();
     }
 }
 drawNiceThing (500, 250, 250, 250);
 
-function drawNiceThing2 (x,y,a,b){
+function drawNiceThing2 (x, y, a, b){
     ctx.strokeStyle = 'green';
-    for(let i = 0; i < canvas.width/2; i+=20){
+    for(let i = 0; i < canvas.width / 2; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x+i, y);
-        ctx.lineTo(a, b-i);
+        ctx.moveTo(x + i, y);
+        ctx.lineTo(a, b - i);
         ctx.stroke();
     }
 }
 drawNiceThing2 (0, 250, 250, 250);
 
-function drawNiceThing3 (x,y,a,b){
+function drawNiceThing3 (x, y, a, b){
     ctx.strokeStyle = 'green';
-    for(let i = 0; i < canvas.width/2; i+=20){
+    for(let i = 0; i < canvas.width / 2; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x+i, y);
-        ctx.lineTo(a, b+i);
+        ctx.moveTo(x + i, y);
+        ctx.lineTo(a, b + i);
         ctx.stroke();
     }
 }
 drawNiceThing3 (0, 250, 250, 250);
 
-function drawNiceThing4 (x,y,a,b){
+function drawNiceThing4 (x, y, a, b){
     ctx.strokeStyle = 'green';
-    for(let i = 0; i < canvas.width/2; i+=20){
+    for(let i = 0; i < canvas.width / 2; i += 20){
         ctx.beginPath();
-        ctx.moveTo(x-i, y);
-        ctx.lineTo(a, b+i);
+        ctx.moveTo(x - i, y);
+        ctx.lineTo(a, b + i);
         ctx.stroke();
     }
 }
