f4cf96caf751863069b22503cb49fa32f2d0790e
Galicz Mihály <galicz555@gmail.com>
2019-09-20 09:52:30 +0200

    Project 2: 1 tile, size-able

--- week-02/day-4/Project2/Drawing project2.ts
+++ week-02/day-4/Project2/Drawing project2.ts
@@ -2,43 +2,50 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 
-let h = 4;
+let checksize = 4;
 
 let x = 0;
-let y = canvas.width/h;
+let newCanvasWidth = canvas.width/checksize;
 
-function drawRectLineplay(x,y,z){
-        ctx.fillRect(x, y, z, z);
-        ctx.strokeStyle = "white";
-        ctx.strokeRect(x, y, z, z);
+function drawRectLineplay(x,y){
+    // ctx.fillRect(x, y, z, z);
+    // ctx.strokeStyle = "white";
+    // ctx.strokeRect(x, y, z, z);
+
     function drawNiceThing (x,y){
         ctx.strokeStyle = 'green';
-        for(let i = 0; i < y; i+=10){
+        for(let i = 0; i < newCanvasWidth; i+=5){
+            // for(let k = 0; i < canvas.height; k+=20){
             ctx.beginPath();
-            ctx.moveTo(x, x+i);
-            ctx.lineTo(x+i, y);
+            ctx.moveTo(x, y+i);
+            ctx.lineTo(x+i,y+newCanvasWidth);
             ctx.stroke();
+            // }
         }
     }
     drawNiceThing (x, y);
-
+    
     function drawNiceThing2 (x,y){
         ctx.strokeStyle = 'purple';
-        for(let i = 0; i < y; i+=10){
+        for(let i = 0; i < newCanvasWidth; i+=5){
+            // for(let k = 0; i < canvas.height; k+=20){
             ctx.beginPath();
-            ctx.moveTo(x+i, x);
-            ctx.lineTo(y, x+i);
+            ctx.moveTo(x+i, y);
+            ctx.lineTo(x+newCanvasWidth, y+i);
             ctx.stroke();
+            // }
         }
     }
     drawNiceThing2 (x, y);
 }
 
+drawRectLineplay(0,0);
 
 
-let checkSize: number = 20;
-for(let i = 0; i < checkSize * 600; i++){
-    for(let j = 0; j < 50; j++){
-        drawRectLineplay(i*checkSize,j*checkSize,1*checkSize);
-    }
-}
+
+// let checkSize: number = 20;
+// for(let i = 0; i < checkSize * 600; i++){
+//     for(let j = 0; j < 50; j++){
+//         drawRectLineplay(i*checkSize,j*checkSize,1*checkSize);
+//     }
+// }
