7a3ca9b96253e55a5f10cd3637b78d55057b9d63
Galicz Mihály <galicz555@gmail.com>
2019-09-20 08:31:55 +0200

    pyramid project

--- week-02/day-4/Project4/drawingProject4.ts
+++ week-02/day-4/Project4/drawingProject4.ts
@@ -7,25 +7,29 @@ const ctx = canvas.getContext('2d');
 
 
 
-function Pyramid(x,y){
+function Pyramid(x, y){
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(x+width,y);
-    ctx.lineTo(x+width/2,y-height);
-    ctx.lineTo(x,y);
+    ctx.lineTo(x + width,y);
+    ctx.lineTo(x + width / 2, y - height);
+    ctx.lineTo(x, y);
     ctx.stroke();
 }
 
 // Pyramid(230,265);
 let width = 20;
-let height = 15;
-let lineCount = 10;
-let lsd = 230
+let height = 17;
+let lineCount = 25;
+let startingWidth = 250;
+let startingHeight = 30;
+
+
 
 
 for(let i = 0; i < lineCount; i++){
-    for(let j = 0; j < (lineCount-i); j++){
-    Pyramid(lsd+i*width,265-j*height);
+    for(let j = 0; j < i ; j++){
+    Pyramid(startingWidth+j*width , startingHeight);
     }
-    lsd += width/2;
+    startingWidth -= width/2;
+    startingHeight += height;
 }
