99467fed0780f94121d13baa0391e11f1b99966b
Galicz Mihály <galicz555@gmail.com>
2019-11-12 16:40:44 +0100

    Did the rocket exercise

--- week-09/backendAPITesting/iamgroot/routes.js
+++ week-09/backendAPITesting/iamgroot/routes.js
@@ -2,6 +2,20 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+let shipStatus = {
+    caliber25: 0,
+    caliber30: 0,
+    caliber50: 0,
+    shipstatus: 0,
+    ready: false
+}
 
 app.get('/groot', function (req, res) {
     let message = req.query.message;
@@ -18,5 +32,77 @@ app.get('/groot', function (req, res) {
         res.json({ "error": "I am Groot!" })
     };
 });
+
+app.get('/yondu', function (req, res) {
+    let distance = req.query.distance;
+    let time = req.query.time;
+    let speed = distance / time;
+    if (distance && time) {
+        let result = {
+            "distance": parseInt(distance),
+            "time": parseInt(time),
+            "speed": speed,
+        }
+        res.status(200);
+        res.json(result);
+    } else {
+        res.status(400);
+        res.json({ "error": "I am Yondu!" })
+    }
+});
+
+app.get('/rocket', function (req, res) {
+    if (shipStatus.shipstatus == 0) {
+        shipStatus.shipstatus = "empty";
+    } else if (shipStatus.shipstatus == 100) {
+        shipStatus.shipstatus = "full";
+    } else if (shipStatus.shipstatus > 100) {
+        shipStatus.shipstatus = "overloaded";
+    }
+    res.status(200);
+    res.json(shipStatus);
+});
+
+app.post('/rocket/fill', function (req, res) {
+    let caliber = req.query.caliber;
+    let amount = req.query.amount;
+    if (caliber == "50" || caliber == "30" || caliber == "25" && amount) {
+        let result = {
+            "received": caliber,
+            "amount": parseInt(amount),
+            "shipstatus": (parseInt(amount) / 12500) * 100 + "%",
+            "ready": false
+        }
+
+        if (shipStatus.shipstatus == "empty") {
+            shipStatus.shipstatus = 0;
+        }
+
+        if (caliber === "25") {
+            shipStatus.caliber25 += parseInt(amount);
+        } else if (caliber === "30") {
+            shipStatus.caliber30 += parseInt(amount);
+        } else if (caliber === "50") {
+            shipStatus.caliber50 += parseInt(amount);
+        }
+        shipStatus.shipstatus += (parseInt(amount) / 12500) * 100;
+
+        if (shipStatus.shipstatus >= 100) {
+            shipStatus.ready = true
+        }
+        res.status(200);
+        res.json(result);
+    } else {
+        res.status(404);
+        res.json({ "error": "You made an error in specification of how much and what" });
+    }
+});
+
+
+
+// function isNaturalNumber(str) {
+//     let pattern = /^(0|([1-9]\d*))$/;
+//     return pattern.test(str);
+// }
 
 module.exports = app;
--- week-09/backendAPITesting/iamgroot/test/routes.js
+++ week-09/backendAPITesting/iamgroot/test/routes.js
@@ -34,3 +34,86 @@ test('Groot test 2', function (t) {
             t.end();
         });
 });
+
+test('yondu', function (t) {
+    request(app)
+        .get('/yondu')
+        .query({ distance: 100.0, time: 10.0 })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+            let result = {
+                "distance": 100.0,
+                "time": 10.0,
+                "speed": 100.0 / 10.0,
+            }
+            t.error(err, 'No error');
+            t.same(res.body, result, 'Tested result and calculation');
+            t.end();
+        });
+});
+
+test('yondu 2', function (t) {
+    request(app)
+        .get('/yondu')
+        .query({})
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end(function (err, res) {
+            t.error(err, 'No error');
+            t.same(res.body, { "error": "I am Yondu!" }, 'Received right status code and json message');
+            t.end();
+        });
+});
+
+test('ship status', function (t) {
+    request(app)
+        .get('/rocket')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+            let status = {
+                "caliber25": 0,
+                "caliber30": 0,
+                "caliber50": 0,
+                "shipstatus": "empty",
+                "ready": false
+            }
+            t.error(err, 'No error');
+            t.equal(res.body.length, status.length, 'Correct number of statuses');
+            t.same(res.body, status, 'Received ship status');
+            t.end();
+        });
+});
+
+test('ship fill', function (t) {
+    request(app)
+        .post('/rocket/fill')
+        .query({ caliber: '50', amount: '5000' })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+            let status = {
+                "received": "50",
+                "amount": 5000,
+                "shipstatus": "40%",
+                "ready": false
+            }
+            t.error(err, 'No error');
+            t.same(res.body, status, 'Received ship status');
+            t.end();
+        });
+});
+
+test('ship fill', function (t) {
+    request(app)
+        .post('/rocket/fill')
+        .query({})
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end(function (err, res) {
+            t.error(err, 'No error');
+            t.same(res.body, { "error": "You made an error in specification of how much and what" }, 'Received ship status');
+            t.end();
+        });
+});
