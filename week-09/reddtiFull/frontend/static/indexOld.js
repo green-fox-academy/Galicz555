7287eb47bf423cdb308bd706e92e11abdfdd230a
Galicz Mihály <galicz555@gmail.com>
2019-11-19 12:13:45 +0100

    Reddit: working get, post, voting, deleting

--- week-09/reddtiFull/frontend/static/indexOld.js
+++ week-09/reddtiFull/frontend/static/indexOld.js
@@ -19,6 +19,35 @@ function getRemoveLinkFromEvent(item) {
     return item.className == " container"
 }
 
+// let redditPostsRequest = new XMLHttpRequest();
+// redditPostsRequest.open("GET", "http://localhost:3000/posts", true);
+
+
+// redditPostsRequest.onload = function () {
+//     let ourData = JSON.parse(redditPostsRequest.responseText);
+//     console.log(ourData.posts.length);
+//     for (let i = 0; i < ourData.posts.length; i++) {
+//         let div = document.createElement("div");
+//         div.innerText = ourData.posts[i].title;
+//         main.appendChild(div);
+
+//     }
+// }
+// redditPostsRequest.send();
+
+// fetch(postsUrl)
+//     .then(((resp) => resp.json()))
+//     .then(function (data) {
+//         for (let i = 0; i < data.posts.length; i++) {
+//             let div = document.createElement("div");
+//             div.innerText = data.posts[i].title;
+//             main.appendChild(div);
+//         }
+//     })
+//     .catch(function (error) {
+//         throw error;
+//     });
+
 fetch(postsUrl)
     .then((resp) => resp.json())
     .then(function (data) {
@@ -41,12 +70,15 @@ fetch(postsUrl)
             voteNum.className += " vote";
             divButtonUp.className += " up";
             divButtonUp.setAttribute("id", user.id);
+            // divButtonUp.setAttribute("onclick", `upVote(${user.id})`); 
             divButtonDown.className += " down";
             divButtonDown.setAttribute("id", user.id);
+            // divButtonDown.setAttribute("onclick", `downVote(${user.id})`); 
             titleLink.className += " tLink";
             titleLink.innerHTML = `<a class="path" href=${user.url}>${user.title}</a>`;
             actions.className += " actions";
             actions.innerHTML = `${ago(user.timestamp)} <a class="actionLink" href=${user.url}>Modify</a><div id=${user.id} class="removeLink">Remove</div>`;
+            // actions.innerHTML = `${ago(user.timestamp)} <a class="actionLink" href=${user.url}>Modify</a><div class="removeLink" onclick=deleteData(${user.id})>Remove</div>`;
             append(divButtons, divButtonUp);
             append(divButtons, voteNum);
             append(divButtons, divButtonDown);
@@ -62,6 +94,8 @@ fetch(postsUrl)
         throw error
     });
 
+
+//                since (timestamp) calculator
 let ago = function (timestamp) {
     let difference = (Date.now() - timestamp) / 1000 / 60;
     if (difference > 60 * 24) {
@@ -75,6 +109,7 @@ let ago = function (timestamp) {
     }
 }
 
+//            event bubbling for upvoting/downvoting/deleting
 main.addEventListener("click", function (event) {
     let voteId = event.target.id;
     let action = event.target.className;
@@ -85,6 +120,7 @@ main.addEventListener("click", function (event) {
             method: 'put'
         });
         number.innerHTML = parseInt(number.innerHTML) + 1;
+        // .then(res => res(window.location.assign('http://localhost:3000')))
     } else if (voteId !== undefined && action == " down") {
         let number = event.path.filter(getVoteNumFromEvent)[0].getElementsByClassName(" vote")[0];
         event.target.className = " downed";
@@ -102,8 +138,79 @@ main.addEventListener("click", function (event) {
     }
 })
 
+//                              gay solution for upvoting/downvoting
+// function upVote(item) {
+//     return fetch("http://localhost:3000/posts" + '/' + item + '/' + "upvote", {
+//         method: 'put'
+//     })
+//         .then(res => res(window.location.assign('http://localhost:3000')))
+// }
+
+// function downVote(item) {
+//     return fetch("http://localhost:3000/posts" + '/' + item + '/' + "downvote", {
+//         method: 'put'
+//     })
+//         .then(res => res(window.location.assign('http://localhost:3000')))
+// }
+
+
+// main.addEventListener("click", function (event) {
+//     let voteId = event.target.id;
+//     let action = event.target.value;
+//     let myRequest = new XMLHttpRequest();
+//     if (voteId !== undefined && action == "up") {
+//         myRequest.open('PUT', `http://localhost:3000/posts/${voteId}/upvote`, true);
+//     }
+//     if (voteId !== undefined && action == "down") {
+//         myRequest.open('PUT', `http://localhost:3000/posts/${voteId}/downvote`, true);
+//         // myRequest.onreadystatechange = function () {
+//         //     if (myRequest.readyState === XMLHttpRequest.DONE && myRequest.status === 200) {
+//         //         let myData = JSON.parse(myRequest.responseText);
+//         //         for (let j = 0; j < myData.length; j++) {
+//         //             console.log(myData[j].score);
+//         //             let p = document.querySelectorAll('.test');
+//         //             let test = Array.from(p);
+//         //             for (let k = 0; k < test.length; k++) {
+//         //                 console.log(test[k].score);
+//         //             }
+
+//         //         }
+//         //     }
+//         // }
+//     }
+//     myRequest.send();
+// });
+
+
+
+// function newArrow(event){
+//     let base = event.target.className;
+//     base = " upped";
+// }
+
+
+
+
+//               deleting onclick function
+// function deleteData(item) {
+//     return fetch("http://localhost:3000/posts" + '/' + item, {
+//         method: 'delete'
+//     })
+//         .then(response => response.json())
+//         .then(res => console.log(res))
+//         .then(res => res(window.location.assign('http://localhost:3000')))
+// }
+
+//                  going to places
 let submitButton = document.querySelector("button");
 function goToAddPost() {
     window.location.assign('http://localhost:3000/add');
 }
 submitButton.addEventListener("click", goToAddPost);
+
+
+
+// let voting = document.querySelector('body');
+// voting.addEventListener('click', function () {
+//     console.log(event)
+// });
